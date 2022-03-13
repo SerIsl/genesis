766b3f60994223e57c40d918e2d89930c427bbfc
== <=>
2022-03-13 22:09:35 +0300

    print_deco dosyası eklendi.

---
+++ print_deco.py
@@ -0,0 +1,16 @@
+def print_deco(function):
+	def wrapper(x, y):
+		bn = ',\n'
+		count = 0
+		a = function(x, y)
+		print('[')
+		for i in a:
+			if count == len(a)-1:
+				print(i, sep="", end="")
+			else:
+				print(i, sep="", end='')
+				print(bn, sep='', end='')
+				count +=1
+		else:
+			print('\n]')
+	return wrapper	
